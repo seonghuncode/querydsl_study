cedc7254a22b04bf8777ce79d22120fc881f21f9
seonghuncode <gnos456@naver.com>
2024-02-27 16:47:04 +0900

    querydsl설정 후정상 실행 여부 테스트

--- src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,42 @@
 package study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Hello;
+import study.querydsl.entity.QHello;
+
+import javax.persistence.EntityManager;
+
 
 @SpringBootTest
+@Transactional
 class QuerydslApplicationTests {
+
+	@Autowired
+	EntityManager em;
 
 	@Test
 	void contextLoads() {
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory query = new JPAQueryFactory(em);
+		QHello qHello = QHello.hello;
+		// QHello qHello = new QHello("h"); //querydsl사용시 쿼리와 관련된 내용은 Q타입을 사용
+
+		//querydsl
+		Hello result = query
+				.selectFrom(qHello)
+				.fetchOne();
+
+		//검증
+		Assertions.assertThat(result).isEqualTo(hello); // result가 hello랑 같은지 테스트
+		Assertions.assertThat(result.getId()).isEqualTo(hello.getId()); //lombok테스트
+
 	}
 
 }
