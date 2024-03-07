5a43356b68b0018a51bb340494d445a431a433c8
seonghuncode <gnos456@naver.com>
2024-03-07 23:05:20 +0900

    순수JPA를 querydal로 변경

--- src/main/java/study/querydsl/QuerydslApplication.java
+++ src/main/java/study/querydsl/QuerydslApplication.java
@@ -1,7 +1,11 @@
 package study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.persistence.EntityManager;
 
 @SpringBootApplication
 public class QuerydslApplication {
@@ -9,5 +13,11 @@ public class QuerydslApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(QuerydslApplication.class, args);
 	}
+
+//	//Spring Bean으로 등록 (자주 사용하는 것것
+//	@Bean
+//	JPAQueryFactory jpaQueryFactory(EntityManager em){
+//		return new JPAQueryFactory(em);
+//	}
 
 }
