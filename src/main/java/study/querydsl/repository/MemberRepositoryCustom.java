6cec5c3ca5858bc62a0a3fb9153b2f3478c067e6
seonghuncode <gnos456@naver.com>
2024-03-09 02:31:58 +0900

    query dsl페이징 연동(전체 카운트를 한번에 조회하는 방법)

--- src/main/java/study/querydsl/repository/MemberRepositoryCustom.java
+++ src/main/java/study/querydsl/repository/MemberRepositoryCustom.java
@@ -1,5 +1,7 @@
 package study.querydsl.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 
@@ -8,4 +10,6 @@ import java.util.List;
 public interface MemberRepositoryCustom {
     //spring data jpa를 사용하면서 직접 구현한 기능을 사용하고 싶을 경우
     List<MemberTeamDto> search(MemberSearchCondition condition);
+    Page<MemberTeamDto> searchPageSimple(MemberSearchCondition condition, Pageable pageable);
+    Page<MemberTeamDto> searchPageComplex(MemberSearchCondition condition, Pageable pageable);
 }
