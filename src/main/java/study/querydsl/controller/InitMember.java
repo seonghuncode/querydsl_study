16c08c049dc6df4ff4baf0fde429ef00e9e2c1da
seonghuncode <gnos456@naver.com>
2024-03-09 01:32:33 +0900

    조회API Controller 개발(applicaion.yml설정을 통해 test환경과 local한경에서 실행되는 파일 분리)

---
+++ src/main/java/study/querydsl/controller/InitMember.java
@@ -0,0 +1,48 @@
+package study.querydsl.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Member;
+import study.querydsl.entity.Team;
+
+import javax.annotation.PostConstruct;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Profile("local")
+@Component
+@RequiredArgsConstructor
+public class InitMember {
+    // application.yml에 설정을 local로 했기 때문에 spring boot에서 실행하면 local 프로파일러가 실행되어 해당 데이터를 DB에 저장하고 시작
+    //데이터를 초기화 하는 부분
+
+    private final InitMemberService initMemberService;
+
+    @PostConstruct
+    public void init(){
+        initMemberService.init();
+    }
+
+    @Component
+    static class InitMemberService {
+
+        @PersistenceContext
+        private EntityManager em;
+
+        @Transactional
+        public void init() {
+            Team teamA = new Team("teamA");
+            Team teamB = new Team("teamB");
+            em.persist(teamA);
+            em.persist(teamB);
+
+            for (int i = 0; i < 100; i++) {
+                Team selectedTeam = i % 2 == 0 ? teamA : teamB;
+                em.persist(new Member("member" + i, i, selectedTeam));
+            }
+        }
+
+    }
+}
