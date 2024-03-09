2bc4666c749eed4b78604e5ee88d0ecc194a6044
seonghuncode <gnos456@naver.com>
2024-03-09 17:47:37 +0900

    spring data jpa가 제공하는 querydsl 기능을 interface지원을 통해 구현

--- src/main/java/study/querydsl/repository/MemberRepository.java
+++ src/main/java/study/querydsl/repository/MemberRepository.java
@@ -1,11 +1,12 @@
 package study.querydsl.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import study.querydsl.entity.Member;
 
 import java.util.List;
 
-public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom, QuerydslPredicateExecutor<Member> {
     //Spring Data JPA를 사용해서 동작하는 로직
 
     // 메서드 이름을 가지고 jpa가 쿼리를 자동으로 만들어주는 전략 (-> select m from Member m where m.username = ?)
